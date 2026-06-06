4d2108d77997f2a74509a684ca8dde4f2f307c19
Mohammed ABOUSAID <abossaid.mohamed.2001@gmail.com>
2026-06-06 17:30:18 +0000

    STEP 2b: Spring Data repositories (User, Category, Ebook)

---
+++ backend/src/main/java/com/library/dls/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.library.dls.repository;
+
+import com.library.dls.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByName(String name);
+}
